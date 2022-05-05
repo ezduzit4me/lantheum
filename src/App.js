846d39de119c7aaa856e393cc3dbaf12e1100768
ezduzit4me <geoffreywarrener@gmail.com>
2022-05-05 20:48:29 +1000

    google tracking

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
 import React, { Component } from 'react';
-  
+import ReactGA from 'react-ga';
 class App extends Component {
   render() {
+    const gaTrackingId="UA-205554703-3";
+      ReactGA.initialize(gaTrackingId);
+      ReactGA,pageview("/");
+
     const myStyle={
         backgroundImage: 
  "url('https://lantheum.s3.ap-southeast-2.amazonaws.com/l_background_v2.jpg')",
