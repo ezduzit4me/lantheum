cb515b1b2edf9688ab3cafb3c3d8898169177ef0
ezduzit4me <geoffreywarrener@gmail.com>
2022-05-14 16:40:48 +1000

    new image

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
 
     const myStyle={
         backgroundImage: 
- "url('https://lantheum.s3.ap-southeast-2.amazonaws.com/l_background_v2.jpg')",
+ "url('https://lantheum.s3.ap-southeast-2.amazonaws.com/lantheum_v5.jpg')",
         height:'100vh',
         marginTop:'-70px',
         fontSize:'50px',
-        backgroundSize: 'cover',
+        backgroundSize: '100% 100%',
         backgroundRepeat: 'no-repeat',
     };
     return (
